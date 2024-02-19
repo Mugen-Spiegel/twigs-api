3bb03c98196c5fe29b846bf88f1a6a55f547e979
Jophat Tamayo <tamay06131993@gmail.com>
2024-02-19 23:39:15 +0800

    move the logic of create user in Create interface

--- app/controllers/user_controller.go
+++ app/controllers/user_controller.go
@@ -5,31 +5,27 @@ import (
 	"strconv"
 
 	"github.com/communi-tree/twigs-api/app/models"
-	"github.com/communi-tree/twigs-api/app/utils/services"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(request *gin.Context) {
-	login := models.Login{}
-	login.Create(request)
-	user := models.User{}
-	user.Create(request, login)
-	tokenString, _ := services.CreateToken(login.Username)
-	request.JSON(
-		http.StatusOK,
-		gin.H{
-			"token": tokenString,
-			"user":  user,
-		},
-	)
-}
 
-// func UserIndex(request *gin.Context) {
-// 	user := []models.User{}
-// 	result := models.DB.Where("id = ?", user_id).Find(&user)
-// 	println(result.RowsAffected)
-// 	request.JSON(http.StatusOK, gin.H{"data": user})
-// }
+	user := models.User{}
+
+	token, e := user.Create(request)
+
+	if e != nil {
+		request.JSON(http.StatusBadRequest, gin.H{"error": e.Error()})
+	} else {
+		request.JSON(
+			http.StatusOK,
+			gin.H{
+				"token": token,
+				"user":  user,
+			},
+		)
+	}
+}
 
 func UserShow(request *gin.Context) {
 	user_id, _ := strconv.Atoi(request.Param("id"))
