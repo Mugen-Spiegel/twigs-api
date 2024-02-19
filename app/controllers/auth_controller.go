e271088b1e63407a1691a410418eacd4f5737ce5
Jophat Tamayo <tamay06131993@gmail.com>
2024-02-19 23:38:16 +0800

    move the logic of authentication in Find interface

--- app/controllers/auth_controller.go
+++ app/controllers/auth_controller.go
@@ -4,37 +4,21 @@ import (
 	"net/http"
 
 	"github.com/communi-tree/twigs-api/app/models"
-	"github.com/communi-tree/twigs-api/app/utils/services"
 	"github.com/gin-gonic/gin"
 )
 
 func LoginHandler(request *gin.Context) {
 	login := models.Login{}
-	if err := request.ShouldBind(&login); err == nil {
-		result := models.DB.Where("username = ?", login.Username).Where("password = ?", login.Password).Find(&login)
-		if result.Error != nil || result.RowsAffected == 0 {
-			request.JSON(
-				http.StatusBadRequest,
-				gin.H{
-					"error": "Not Found",
-				},
-			)
-		} else {
-			tokenString, err := services.CreateToken(login.Username)
-			if err != nil {
-				panic("Invalid Strings")
-			}
-			request.JSON(
-				http.StatusOK,
-				gin.H{
-					"token": tokenString,
-					"user":  login,
-				},
-			)
-		}
-
+	token, user, e := login.Find(request)
+	if e != nil {
+		request.JSON(http.StatusBadRequest, gin.H{"error": e.Error()})
 	} else {
-		request.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		request.JSON(
+			http.StatusOK,
+			gin.H{
+				"token": token,
+				"user":  user,
+			},
+		)
 	}
-
 }
